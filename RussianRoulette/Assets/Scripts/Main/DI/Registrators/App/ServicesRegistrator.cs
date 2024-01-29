fffa7eff140c97699ef6daea53db0b0832647a58
TatRat <65038336+TatRat@users.noreply.github.com>
2024-01-29 23:02:33 +0300

    di examples

---
+++ RussianRoulette/Assets/Scripts/Main/DI/Registrators/App/ServicesRegistrator.cs
@@ -0,0 +1,16 @@
+﻿using TatRat.Main.DI;
+using TatRat.Messages;
+using TatRat.SceneLoading;
+using VContainer;
+
+namespace TatRat.Main
+{
+    internal class ServicesRegistrator : AppRegistrator
+    {
+        public override void Register(IContainerBuilder builder)
+        {
+            builder.Register<SceneLoadService>(Lifetime.Singleton).AsImplementedInterfaces();
+            builder.Register<MessageService>(Lifetime.Singleton).AsImplementedInterfaces();
+        }
+    }
+}
